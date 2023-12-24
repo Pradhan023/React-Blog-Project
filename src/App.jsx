c4574ce13ec3c9b6dd6cbbce87fc802867dd5b48
Anish Pradhan <anishpradha5523@gmail.com>
2023-12-24 20:43:38 +0530

    Blog Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import React, { Suspense, lazy } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Footer from './Component/Footer/Footer'
 import './Component/Loader style/Loader.css'
+import axios from 'axios'
+import DataProvide from './Component/Router/DataProvide'
+import Store from './Component/DataStore'
 
-const Compdisplay = lazy(()=> import('./Component/Router/DataProvide'));
 
 const App = () => {
-  return (
-    <div>
-      <Suspense fallback={<div class="lds-dual-ring"></div>}>
-      <Compdisplay/>
-      <Footer/>
-      </Suspense>
-    </div>
-  )
+  const [api,setApi] = useState([])
+
+    useEffect(()=>{
+        axios.get('https://blog-api-hqh6.onrender.com/api/blogsite/main/data').then((res)=> setApi(res.data));
+    },[])
+
+    const [Data] = useState(Store)
+
+    if(api.length === 0){
+      return(
+        <div class="lds-dual-ring"></div>
+      )
+    }
+    else{
+      return (
+        <div>
+          <Data.Provider value={api}>
+          <DataProvide/>
+          <Footer/>
+          </Data.Provider>
+        </div>
+      )
+    }
 }
 
 export default App
--- src/Component/Router/DataProvide.jsx
+++ src/Component/Router/DataProvide.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import Data from '../DataStore'
+import React from 'react'
 import {Routes , Route} from 'react-router-dom'
 import Home from './Home'
 import Bollywood from './Bollywood'
@@ -7,20 +6,11 @@ import Hollywood from './Hollywood'
 import Technology from './Technology'
 import Fitness from './Fitness'
 import Newpage from './Newpage'
-import axios from 'axios'
 import Education from './Educataion'
 
 const DataProvide = () => {
-    const [api,setApi] = useState('')
-
-    useEffect(()=>{
-        axios.get('https://blog-api-hqh6.onrender.com/api/blogsite/main/data').then((res)=> setApi(res.data));
-    },[])
-
-   
   return (
     <div>
-        <Data.Provider value={api}>
         <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/bollywood' element={<Bollywood/>} />
@@ -31,7 +21,6 @@ const DataProvide = () => {
         <Route path='/news/:id/:category' element={<Newpage/>} />
         <Route path='/morenews/:id/:category' element={<Newpage/>} />
       </Routes>
-        </Data.Provider>
     </div>
   )
 }
